7c50bf3540ccae2789d3c1149738dbedc0e6b663
kimsunghyun <berserk.gahz@gmail.com>
2023-07-03 17:37:47 +0900

    'step1_스와이프_옵션_설정'

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper("main", {
+  loop: true,
+  grabCursor: true,
+  centeredSlides: true,
+  direction: "horizontal", //"vertical",
+  speed: 500,
+  spaceBetween: 20,
+  slidesPerView: 3,
+});
